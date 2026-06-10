e834e5df13261d115e764ec81cb62a4a12c27501
“HanChaehui” <hch8344@naver.com>
2026-06-10 11:09:42 +0900

    inheritance

--- java-part-2/src/main/java/G_inheritance.java
+++ java-part-2/src/main/java/G_inheritance.java
@@ -28,5 +28,17 @@
 public class G_inheritance {
     static void main(String[] args) {
 
+        G_dog dog = new G_dog();
+        dog.kind = "korean dog";
+        dog.eat();
+        dog.walk();
+        dog.bark(); // 확장
+
+        G_bird bird = new G_bird();
+        bird.kind = "american bird";
+        bird.eat();
+        bird.walk();
+        bird.fly();
+
     }
 }
