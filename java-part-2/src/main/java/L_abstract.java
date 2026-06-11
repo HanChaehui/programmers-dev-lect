3ec85ba5fb180ac8c7a7d925077ef142a7fbbb30
“HanChaehui” <hch8344@naver.com>
2026-06-11 10:03:49 +0900

    polymorphism

--- java-part-2/src/main/java/L_abstract.java
+++ java-part-2/src/main/java/L_abstract.java
@@ -1,57 +1,48 @@
 
-// * 추상 클래스
-// 추상 클래스는 인스턴스화할 수 없는 클래스이다.
-// 즉, 추상클래 자체로 객체를 생성할 수 없으며, 반드시 이 클래스를 상속받은 자식 클래스를 통해서만 객체를 생성할 수 있다.
-// 추상 클래스는 추상 메서드뿐만 아니라 일반 메서드도 포함할 수 있으며, 필드(멤버 변수)를 가질 수도 있다.
+// * 다형성
+// 동일한 인터페이스나 부모 클래스를 공유하는 여러 객체들이 각자 다른 방식으로 동작하도록 할 수 있는 기능을 말한다.
+// 다형성은 "하나의 인터페이스로 여러 형태의 객체를 처리할 수 있다"는 의미를 가지고 있다.
+// 이를 통해 코드의 유연성과 확장성을 크게 향상시킬 수 있다.
 
-// * 추상 메서드
-// 추상 메서드는 메서드의 선언만 존재하고, 구현(메서드이 몸체)이 없는 메서드이다.
-// 추상 메서드는 추상 클래스 내에서 정의되며, 이 메서드를 상속받은 자식 클래스는 반드시 이 메서드를 오버라이딩하여 구현해야 한다.
-
-// * 요약
-// 추상 클래스 : 인스턴스화할 수 없으며, 공통된 속성이나 메서드를 정의하고, 일부 메서드는 자식 클래스에서 구현하도록 강제한다.
-// 추상 메서드 : 선언만 있고, 구현은 없으며, 반드시 자식 클래스에서 오버라이딩되어야 한다.
-
-// * 단점
-
-abstract class L_animal {
-
-    // 추상 메서드 : 하위 클래스가 이 메서드를 구현해야함(오버라이딩, 재정의) -> 강제
-    abstract void makeSound();
-
-    // 일반 메서드 : 모든 하위 클래스에서 동일하게 동작함
-    void breathe() {
-        System.out.println("animal breathing");
+class K_animal {
+    // 부모클래스에서 정의된 메서드
+    public void sound() {
+        System.out.println("Animal sound");
     }
 }
 
-class L_dog extends L_animal {
+class K_dog extends K_animal {
+
+    public void fetch() {
+        System.out.println("Dog fetches a ball");
+    }
+
     @Override
-    void makeSound() {
-        System.out.println("dog sound");
+    public void sound() {
+        System.out.println("Dog sound");
     }
 }
 
-class L_cat extends L_animal {
+class K_cat extends K_animal {
+
     @Override
-    void makeSound() {
-        System.out.println("cat sound");
+    public void sound() {
+        System.out.println("Cat sound");
     }
 }
 
-public class L_abstract {
+public class K_polymorphism {
     static void main(String[] args) {
-        // 오류 : 추상 클래스는 인스턴스화할 수 없음
-//        L_animal temp = new L_animal();
 
-        L_animal dog = new L_dog();
-        L_animal cat = new L_cat();
+        K_animal myAnimal = new K_animal(); // K_animal 타입의 객체
+        K_animal myDog = new K_dog(); // Dog타입의 객체, Animal타입으로 업캐스팅
+        K_animal myCat = new K_cat(); // Cat타입의 객체, Animal타입으로 업캐스팅
 
-        dog.makeSound();
-        dog.breathe();
+        myAnimal.sound();
+        myDog.sound(); // 런타임 시점에 Dog의 sound()호출
+        myCat.sound(); // 런타임 시점에 Cat의 sound()호출
 
-        cat.makeSound();
-        cat.breathe();
-
+        // 다운캐스팅을 통해 다시 Dog 타입으로 변환
+//        K_dog myDog2 = (K_dog) myAnimal;
     }
 }
